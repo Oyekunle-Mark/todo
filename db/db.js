c3a59043cac23376b40509dcce19bbcefe0c2726
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-01-25 23:45:23 +0100

    The file data storage

---
+++ db/db.js
@@ -0,0 +1,14 @@
+const todo = [
+	{
+		"id": 1,
+		"title": "My Title",
+		"description": "My Todo Description"
+	},
+	{
+		"id": 2,
+		"title": "Andela",
+		"description": "The andela developers challenge"
+	}
+]
+
+export default todo
